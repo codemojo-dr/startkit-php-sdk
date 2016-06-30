1dbc5fbb057c6fcda523c7ea430005ae490cf6ec
Shoaib <shoaib@codemojo.io>
2016-06-30 05:42:52 +0530

    Updated group_id to category_id

--- src/CodeMojo/Client/Services/GamificationService.php
+++ src/CodeMojo/Client/Services/GamificationService.php
@@ -60,16 +60,16 @@ class GamificationService
     /**
      * @param $user_id
      * @param $action_id
-     * @param $group_id
+     * @param $category_id
      * @return bool
      * @throws \CodeMojo\Client\Http\InvalidArgumentException
      * @throws \CodeMojo\OAuth2\Exception
      */
-    public function addAchievements($user_id, $action_id, $group_id = null){
+    public function addAchievements($user_id, $action_id, $category_id = null){
         $url = $this->authenticationService->getServerEndPoint() . Endpoints::VERSION . Endpoints::BASE_GAMIFICATION . Endpoints::GAMIFICATION_ACHIEVEMENTS;
 
         $params = array(
-            "customer_id" => $user_id, "action_id" => $action_id, "id" => $group_id
+            "customer_id" => $user_id, "action_id" => $action_id, "id" => $category_id
         );
 
         $result = $this->authenticationService->getTransport()->fetch($url, $params,'PUT', array(), 0);
